62dbfe69465b71ef5849822e267fd502ccf92de3
nitta9923 <puudai.0509@gmail.com>
2025-05-01 19:20:09 +0900

    Viteの設定追加、ESMベースなのでエイリアス追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,8 +14,13 @@ export default defineConfig({
     host: true,
     port: 5173,
     watch: {
-      usePolling: true,   // ファイルの変更をポーリング監視する設定
+      usePolling: true, // ファイルの変更をポーリング監視する設定
     },
     strictPort: true,
-  }
-})
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
